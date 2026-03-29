12c46a17d9d55133e944163a8af543a5d54e70f5
sandesh gawas <sandeshgawas918@gmail.com>
2026-03-29 21:16:51 +0530

    build done

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import TransactionDashboard from "./components/TransactionDashboard";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center bg-zinc-50 font-sans mt-10 mb-20 dark:bg-black">
+    <div className="flex flex-col items-center justify-center font-sans mt-10 mb-20 dark:bg-black">
       <h1 className=" text-2xl font-semibold">Transaction Dashboard with API Integration</h1>
       <TransactionDashboard/>
     </div>
